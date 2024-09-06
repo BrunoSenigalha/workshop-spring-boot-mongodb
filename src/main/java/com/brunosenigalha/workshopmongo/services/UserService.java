92b57488521dae2925b59e3efc44b813a4b40bd3
Bruno Senigalha <brsenigalha@gmail.com>
2024-09-06 10:13:31 -0300

    User getById

--- src/main/java/com/brunosenigalha/workshopmongo/services/UserService.java
+++ src/main/java/com/brunosenigalha/workshopmongo/services/UserService.java
@@ -2,10 +2,13 @@ package com.brunosenigalha.workshopmongo.services;
 
 import com.brunosenigalha.workshopmongo.domain.User;
 import com.brunosenigalha.workshopmongo.repositories.UserRepository;
+import com.brunosenigalha.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +18,10 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repository.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Invalid id"));
     }
 }
---
+++ src/main/java/com/brunosenigalha/workshopmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,7 @@
+package com.brunosenigalha.workshopmongo.services.exception;
+
+public class ObjectNotFoundException extends RuntimeException {
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
