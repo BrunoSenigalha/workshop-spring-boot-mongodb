9e154b71fe6c47a7ee87ed5c6bce694288b93f4b
Bruno Senigalha <brsenigalha@gmail.com>
2024-09-06 10:35:36 -0300

    Insertion of User through POST

--- src/main/java/com/brunosenigalha/workshopmongo/services/UserService.java
+++ src/main/java/com/brunosenigalha/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.brunosenigalha.workshopmongo.services;
 
 import com.brunosenigalha.workshopmongo.domain.User;
+import com.brunosenigalha.workshopmongo.dto.UserDTO;
 import com.brunosenigalha.workshopmongo.repositories.UserRepository;
 import com.brunosenigalha.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id){
         Optional<User> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Invalid id"));
+    }
+
+    public User insert(User obj){
+        return repository.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
