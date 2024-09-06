ab60db007fa49758ccc20eb6bd895f49ccc13d3d
Bruno Senigalha <brsenigalha@gmail.com>
2024-09-06 09:11:51 -0300

    UserDTO

--- src/main/java/com/brunosenigalha/workshopmongo/resources/UserResource.java
+++ src/main/java/com/brunosenigalha/workshopmongo/resources/UserResource.java
@@ -1,16 +1,14 @@
 package com.brunosenigalha.workshopmongo.resources;
 
 import com.brunosenigalha.workshopmongo.domain.User;
+import com.brunosenigalha.workshopmongo.dto.UserDTO;
 import com.brunosenigalha.workshopmongo.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -20,11 +18,11 @@ public class UserResource {
     @Autowired
     private UserService service;
 
-//    @RequestMapping(method = RequestMethod.GET)
+    //    @RequestMapping(method = RequestMethod.GET)
     @GetMapping
-    public ResponseEntity<List<User>> findAll() {
+    public ResponseEntity<List<UserDTO>> findAll() {
         List<User> list = service.findAll();
-//        list.addAll(Arrays.asList(user1, user2));
-        return ResponseEntity.ok().body(list) ;
+        List<UserDTO> listDto = list.stream().map(UserDTO::new).toList();
+        return ResponseEntity.ok().body(listDto);
     }
 }
