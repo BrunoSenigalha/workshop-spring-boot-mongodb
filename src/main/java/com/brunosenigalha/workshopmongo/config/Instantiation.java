5ef0a3b1f7719e5dd56dc9097f424e610ec59304
Bruno Senigalha <brsenigalha@gmail.com>
2024-09-09 13:03:14 -0300

    Endpoint to return the User Posts

--- src/main/java/com/brunosenigalha/workshopmongo/config/Instantiation.java
+++ src/main/java/com/brunosenigalha/workshopmongo/config/Instantiation.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.TimeZone;
